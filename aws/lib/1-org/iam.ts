d616903d3e62e5fffa96d7883b45a1969cc392ad
Pedro Miranda <me@pedromc.tech>
2022-06-25 17:55:05 -0500

    DevOps admin permissions

--- aws/lib/1-org/iam.ts
+++ aws/lib/1-org/iam.ts
@@ -6,8 +6,12 @@ export class IAM {
     public readonly devOpsAdminGroup!: aws.iam.Group
 
     constructor(private readonly environment: string) {
-        this.devOpsAdminRole = this.setDevOpsAdminRole()
-        this.devOpsAdminGroup = this.setDevOpsAdminGroup(this.devOpsAdminRole)
+        const devOpsAdminRole = this.setDevOpsAdminRole()
+        const devOpsAdminGroup = this.setDevOpsAdminGroup(this.devOpsAdminRole)
+        this.setDevOpsAdminRolePolicy(devOpsAdminRole)
+        
+        this.devOpsAdminRole = devOpsAdminRole
+        this.devOpsAdminGroup = devOpsAdminGroup
     }
 
 
@@ -15,6 +19,29 @@ export class IAM {
         new aws.iam.Role('devops-admin', {
             name: `${this.environment}-devops-admin`,
             assumeRolePolicy: aws.getCallerIdentity().then(_ => aws.iam.assumeRolePolicyForPrincipal({ AWS: `arn:aws:iam::${_.accountId}:root` }))
+        })
+
+
+    private setDevOpsAdminRolePolicy = (role: aws.iam.Role): aws.iam.RolePolicy =>
+        new aws.iam.RolePolicy('devops-admin', {
+            role,
+            policy: aws.iam.getPolicyDocumentOutput({
+                statements: [
+                    {
+                        actions: [
+                            "iam:*",
+                            "s3:*",
+                            "ec2:*",
+                            "kms:*",
+                            "eks:*",
+                            "ssm:*",
+                            "autoscaling:*",
+                            "cloudformation:*"
+                        ],
+                        resources: ["*"],
+                    }
+                ],
+            }).json
         })
 
 
